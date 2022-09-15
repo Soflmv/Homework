908caf02ae57ee0d71abc14cdfcc8b0b7390bac9
Sofl069 <108791632+Sofl069@users.noreply.github.com>
2022-09-15 15:01:29 +0300

    add function sorting corrected

--- Flask/app.py
+++ Flask/app.py
@@ -1,34 +1,29 @@
+import time, random
 from flask import Flask, render_template
-import time
 
 
 app = Flask(__name__)
 
 
 # Пузырьковая сортировка
-n = 36
-mas = [3, 7, 5, 2, 8, 1, 5, 7, 5, 25, 13, 16, 23, 14, 6, 4, 3, 4, 7, 11, 9, 6, 7, 2, 1, 8, 33, 6, 3, 2, 4, 7, 8, 5, 4, 1]
-count = 0
+def bubble_sort(mas):
+    length = len(mas)
+    count = 0
 
-startTime = time.time()
+    for i in range(length):
+        for j in range(0, length-i-1):
+            # print(f"Сейчас сравниваем {mas[i]} и {mas[i + 1]}")
+            if mas[j] > mas[j + 1]:
+                count += 1
+                mas[j], mas[j + 1] = mas[j + 1], mas[j]
 
-for run in range(n - 1):
-    for i in range(n - 1):
-        if mas[i] > mas[i + 1]:
-            count += 1
-            mas[i], mas[i + 1] = mas[i + 1], mas[i]
 
-
-time.sleep(1)
-endTime = time.time()
-totalTime1 = (endTime - startTime)
+n = 20
+mas = [random.randint(1, 20) for i in range(n)]
+bubble_sort(mas)
 
 
 # Вставками
-lst = [3, 7, 5, 2, 8, 1, 5, 7, 5, 25, 13, 16, 23, 14, 6, 4, 3, 4, 7, 11, 9, 6, 7, 2, 1, 8, 33, 6, 3, 2, 4, 7, 8, 5, 4, 1]
-startTime = time.time()
-
-
 def insertion_sort(lst):
     for item in range(1, len(lst)):
         current_value = lst[item]
@@ -41,97 +36,116 @@ def insertion_sort(lst):
     return lst
 
 
-time.sleep(1)
-endTime = time.time()
-totalTime2 = (endTime - startTime)
+n = 20
+lst = [random.randint(1, 20) for i in range(n)]
+insertion_sort(lst)
 
 
 # Выбором
-lst = [3, 7, 5, 2, 8, 1, 5, 7, 5, 25, 13, 16, 23, 14, 6, 4, 3, 4, 7, 11, 9, 6, 7, 2, 1, 8, 33, 6, 3, 2, 4, 7, 8, 5, 4, 1]
-startTime = time.time()
-
-for i in range(0, len(lst)-1):
-    min_index = i
-    for j in range(i+1, len(lst)):
-        if lst[min_index] > lst[j]:
-            min_index = j
-    if min_index != i:
-        temp = lst[i]
-        lst[i] = lst[min_index]
-        lst[min_index] = temp
+def selection_sort(obj):
+    for i in range(0, len(obj)-1):
+        smallest = i
+        for j in range(i+1, len(obj)):
+            if obj[j] < obj[smallest]:
+                smallest = j
+        obj[i], obj[smallest] = obj[smallest], obj[i]
 
 
-time.sleep(1)
-endTime = time.time()
-totalTime3 = (endTime - startTime)
+n = 20
+obj = [random.randint(1, 20) for i in range(n)]
+selection_sort(obj)
 
 
 # Слиянием
-startTime = time.time()
-
-
-def merge_to_list(a, b):
-    c = []
-    i = j = 0
-    while i < len(a) and j < len(b):
-        if a[i] < b[j]:
-            c.append(a[i])
+def merge_sort(nums):
+    if len(nums) > 1:
+        mid = len(nums)//2
+        left = nums[:mid]
+        right = nums[mid:]
+        merge_sort(left)
+        merge_sort(right)
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                nums[k] = left[i]
+                i += 1
+            else:
+                nums[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            nums[k] = left[i]
             i += 1
-        else:
-            c.append(b[j])
+            k += 1
+        while j < len(right):
+            nums[k] = right[j]
             j += 1
-    if i < len(a):
-        c += a[i:]
-    if j < len(b):
-        c += b[j:]
-    return c
+            k += 1
 
 
-def merge_sort(s):
-    if len(s) == 1:
-        return s
-    middle = len(s)//2
-    left = merge_sort(s[:middle])
-    rigth = merge_sort(s[middle:])
-
-    return merge_to_list(left, rigth)
-
-
-lst = [3, 7, 5, 2, 8, 1, 5, 7, 5, 25, 13, 16, 23, 14, 6, 4, 3, 4, 7, 11, 9, 6, 7, 2, 1, 8, 33, 6, 3, 2, 4, 7, 8, 5, 4, 1]
-
-
-time.sleep(1)
-endTime = time.time()
-totalTime4 = (endTime - startTime)
+n = 20
+nums = [random.randint(1, 20) for el in range(n)]
+merge_sort(nums)
 
 
 # Быстрая
-startTime = time.time()
+def quick_sort(num):
 
-
-def quick_sort(s):
-
-    if len(s) <= 1:
-        return s
-    elem = s[0]
-    left = list(filter(lambda x: x < elem, s))
-    center = [i for i in s if i == elem]
-    right = list(filter(lambda x: x > elem, s))
+    if len(num) <= 1:
+        return num
+    elem = num[0]
+    left = list(filter(lambda x: x < elem, num))
+    center = [i for i in num if i == elem]
+    right = list(filter(lambda x: x > elem, num))
 
     return quick_sort(left) + center + quick_sort(right)
 
 
-lst = [3, 7, 5, 2, 8, 1, 5, 7, 5, 25, 13, 16, 23, 14, 6, 4, 3, 4, 7, 11, 9, 6, 7, 2, 1, 8, 33, 6, 3, 2, 4, 7, 8, 5, 4, 1]
-
-time.sleep(1)
-endTime = time.time()
-totalTime5 = (endTime - startTime)
+n = 20
+num = [random.randint(1, 20) for j in range(n)]
+merge_sort(num)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', total=totalTime1, total2=totalTime2, total3=totalTime3, total4=totalTime4,
-                           total5=totalTime5)
+    # Сортировка пузырьком
+    startTime = time.time()
+    bubble_sort(mas)
+    time.sleep(1)
+    endTime = time.time()
+    totalTime1 = (endTime - startTime).__round__(6)
+
+    # Вставками
+    startTime = time.time()
+    insertion_sort(lst)
+    time.sleep(1)
+    endTime = time.time()
+    totalTime2 = (endTime - startTime).__round__(6)
+
+    # Выбором
+    startTime = time.time()
+    selection_sort(obj)
+    time.sleep(1)
+    endTime = time.time()
+    totalTime3 = (endTime - startTime).__round__(6)
+
+    # Слиянием
+    startTime = time.time()
+    merge_sort(nums)
+    time.sleep(1)
+    endTime = time.time()
+    totalTime4 = (endTime - startTime).__round__(6)
+
+
+    # Быстрая
+    startTime = time.time()
+    quick_sort(num)
+    time.sleep(1)
+    endTime = time.time()
+    totalTime5 = (endTime - startTime).__round__(6)
+
+    return render_template('index.html',
+                           total=totalTime1, total2=totalTime2, total3=totalTime3, total4=totalTime4, total5=totalTime5)
 
 
 if __name__ == "__main__":
